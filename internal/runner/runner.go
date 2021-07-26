16464827b258f020220cb25dcb9f5b37d8671a22
Sajad Parra <parrasajad@gmail.com>
2021-07-26 17:38:06 +0530

    remove intercept and collaborator

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,17 +2,11 @@ package runner
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"path"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/pkg/errors"
-	"github.com/projectdiscovery/collaborator"
 	"github.com/projectdiscovery/gologger"
-	"github.com/projectdiscovery/notify/pkg/engine"
 	"github.com/projectdiscovery/notify/pkg/providers"
 	"github.com/projectdiscovery/notify/pkg/types"
 	"gopkg.in/yaml.v2"
@@ -20,20 +14,13 @@ import (
 
 // Runner contains the internal logic of the program
 type Runner struct {
-	options    *types.Options
-	burpcollab *collaborator.BurpCollaborator
-	notifier   *engine.Notify
-	providers  *providers.Client
+	options   *types.Options
+	providers *providers.Client
 }
 
 // NewRunner instance
 func NewRunner(options *types.Options) (*Runner, error) {
-	burpcollab := collaborator.NewBurpCollaborator()
 
-	notifier, err := engine.NewWithOptions(options)
-	if err != nil {
-		return nil, err
-	}
 	var providerOptions providers.Options
 
 	if options.ProviderConfig == "" {
@@ -41,7 +28,7 @@ func NewRunner(options *types.Options) (*Runner, error) {
 		if err != nil {
 			return nil, err
 		}
-		options.ProviderConfig = path.Join(home, "/.config/notify/provider-config.yaml")
+		options.ProviderConfig = path.Join(home, types.DefaultProviderConfigLocation)
 		gologger.Print().Msgf("Using default provider config: %s\n", options.ProviderConfig)
 	}
 
@@ -62,7 +49,7 @@ func NewRunner(options *types.Options) (*Runner, error) {
 		return nil, err
 	}
 
-	return &Runner{options: options, burpcollab: burpcollab, notifier: notifier, providers: prClient}, nil
+	return &Runner{options: options, providers: prClient}, nil
 }
 
 // Run polling and notification
@@ -123,90 +110,11 @@ func (r *Runner) Run() error {
 		}
 		os.Exit(0)
 	}
-
-	// otherwise works as long term collaborator poll and notify via webhook
-	// If BIID passed via cli
-	if r.options.BIID != "" {
-		gologger.Print().Msgf("Using BIID: %s", r.options.BIID)
-		r.burpcollab.AddBIID(r.options.BIID)
-	}
-
-	if r.options.BIID == "" {
-		return fmt.Errorf("BIID not specified or not found")
-	}
-
-	err := r.burpcollab.Poll()
-	if err != nil {
-		return err
-	}
-
-	pollTime := time.Duration(r.options.Interval) * time.Second
-	for {
-		time.Sleep(pollTime)
-		//nolint:errcheck // silent fail
-		r.burpcollab.Poll()
-
-		for _, httpresp := range r.burpcollab.RespBuffer {
-			for i := range httpresp.Responses {
-				resp := httpresp.Responses[i]
-				var at int64
-				at, _ = strconv.ParseInt(resp.Time, 10, 64)
-				atTime := time.Unix(0, at*int64(time.Millisecond))
-				switch resp.Protocol {
-				case "http", "https":
-					rr := strings.NewReplacer(
-						"{{protocol}}", strings.ToUpper(resp.Protocol),
-						"{{from}}", resp.Client,
-						"{{time}}", atTime.String(),
-						"{{request}}", resp.Data.RequestDecoded,
-						"{{response}}", resp.Data.ResponseDecoded,
-					)
-
-					msg := rr.Replace(r.options.HTTPMessage)
-					gologger.Print().Msgf(msg)
-
-					//nolint:errcheck // silent fail
-					r.notifier.SendNotification(msg)
-				case "dns":
-					rr := strings.NewReplacer(
-						"{{type}}", resp.Data.RequestType,
-						"{{domain}} ", resp.Data.SubDomain,
-						"{{from}}", resp.Client,
-						"{{time}}", atTime.String(),
-						"{{request}}", resp.Data.RawRequestDecoded,
-					)
-					msg := rr.Replace(r.options.DNSMessage)
-					gologger.Print().Msgf(msg)
-
-					//nolint:errcheck // silent fail
-					r.notifier.SendNotification(msg)
-				case "smtp":
-					rr := strings.NewReplacer(
-						"{{from}}", resp.Client,
-						"{{time}}", atTime.String(),
-						"{{sender}}", resp.Data.SenderDecoded,
-						"{{recipients}}", strings.Join(resp.Data.RecipientsDecoded, ","),
-						"{{message}}", resp.Data.MessageDecoded,
-						"{{conversation}}", resp.Data.ConversationDecoded,
-					)
-					msg := rr.Replace(r.options.SMTPMessage)
-					gologger.Print().Msgf(msg)
-
-					//nolint:errcheck // silent fail
-					r.notifier.SendNotification(msg)
-				}
-			}
-		}
-
-		r.burpcollab.Empty()
-	}
+	return errors.New("notify works with stdin or file using -data flag")
 }
 
 func (r *Runner) sendMessage(msg string) error {
-	rr := strings.NewReplacer(
-		"{{data}}", msg,
-	)
-	msg = rr.Replace(r.options.CLIMessage)
+
 	if len(msg) > 0 {
 		gologger.Print().Msgf(msg)
 		err := r.providers.Send(msg)
@@ -218,6 +126,4 @@ func (r *Runner) sendMessage(msg string) error {
 }
 
 // Close the runner instance
-func (r *Runner) Close() {
-	r.burpcollab.Empty()
-}
+func (r *Runner) Close() {}
