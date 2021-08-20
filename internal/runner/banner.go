647a5d42e36faf5a671fd04bcaed62a495970616
sandeep <sandeep@projectdiscovery.io>
2021-08-20 23:39:01 +0530

    version update

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -8,12 +8,12 @@ const banner = `
              __  _ ___    
   ___  ___  / /_(_) _/_ __
  / _ \/ _ \/ __/ / _/ // /
-/_//_/\___/\__/_/_/ \_, / 0.0.2 
+/_//_/\___/\__/_/_/ \_, / v1.0.0
                    /___/  
 `
 
 // Version is the current version
-const Version = `0.0.2`
+const Version = `1.0.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
